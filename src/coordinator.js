b3c31b8baad36e062a0bc6e7d00e63081de4d9a4
Daniel Steele <danielsteele@hotmail.co.uk>
2019-02-21 00:17:08 +0000

    Detect ground failure state

--- src/coordinator.js
+++ src/coordinator.js
@@ -2,24 +2,28 @@ export default class Coordinator {
 
   static get groundHeight() { return 40 }
 
+  get topOfGround() { return this.windowHeight - Coordinator.groundHeight }
+
   constructor(player, game) {
     this.player = player
     this.game = game
     this.ticks = 0
+    this.windowWidth = game.windowWidth
+    this.windowHeight = game.windowHeight
   }
 
   tick() {
     this.ticks += 1
     this.drawBackground()
     this.renderPlayer()
+    if (this.detectFailure()) { return this.end() }
     this.setScore()
-    this.detectFailure()
+    this.player.accellerate()
   }
 
   renderPlayer() {
     this.game.setFillColor(252, 247, 93)
     this.game.ellipse(this.player.x, this.player.y, this.player.size)
-    this.player.accellerate()
   }
 
   drawBackground() {
@@ -27,8 +31,8 @@ export default class Coordinator {
     this.game.setFillColor(117, 71, 55)
     this.game.rect(
       0,
-      this.game.windowHeight - Coordinator.groundHeight,
-      this.game.windowWidth,
+      this.windowHeight - Coordinator.groundHeight,
+      this.windowWidth,
       Coordinator.groundHeight
     )
   }
@@ -43,7 +47,17 @@ export default class Coordinator {
   }
 
   detectFailure() {
+    if (this.hasHitGround()) { return true }
+    return false
+  }
 
+  hasHitGround() {
+    return this.player.y + this.player.size / 2 > this.topOfGround
+  }
+
+  end() {
+    console.log('Game over')
+    return this.game.noLoop()
   }
 
 }
